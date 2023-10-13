45071e6e67abbe0710066befc34ea1b3346baa5a
Rahul <adityakashyap2501@gmail.com>
2023-10-13 23:53:04 +0530

    ADDED MAX and MIN in Exp3

--- Exp3.sql
+++ Exp3.sql
@@ -6,6 +6,12 @@ WHERE Salary = 200000;
 -- AVG()
 SELECT AVG(salary) FROM faculties;
 
+-- MIN()
+SELECT MIN(salary) FROM faculties;
+
+-- MAX()
+SELECT MAX(salary) FROM faculties;
+
 -- SUM()
 SELECT SUM(salary) FROM faculties;
 
